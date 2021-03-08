a095f63dccc208c11469455e5b3f44f36cfdee8d
Mohammad Gupron <ghuvrons@gmail.com>
2021-03-08 21:58:39 +0700

    initial

---
+++ Jati/Base/Route.py
@@ -0,0 +1,222 @@
+
+import re, json
+
+class Route:
+    def group(self, *arg, **args):
+        key = ('url', 'middleware', 'group', )
+        obj = {'url':'/', 'middleware':[], 'group':[]}
+        obj.update(args)
+        i = 0
+        for val in arg:
+            if i > len(key): break
+            obj[key[i]] = val
+            i+=1
+        return {
+            'url': str(obj['url']), 
+            'middleware': obj['middleware'], 
+            'sub': [x for x in obj['group']]
+        }
+    #respond for ws
+    def route(self, *arg, **args):
+        key = ('url', 'controller', 'method', 'middleware', 'respond', )
+        obj = {'url':'/', 'controller':None, 'method':['get', 'post'], 'middleware':[], 'respond':None}
+        obj.update(args)
+        i = 0
+        for val in arg:
+            if i > len(key): break
+            obj[key[i]] = val
+            i+=1
+        return {
+            'url': str(obj['url']), 
+            'controller': str(obj['controller']), 
+            'methods': obj['method'], 
+            'middleware': obj['middleware'], 
+            "respond": obj['respond'] if not obj['respond'] else str(obj['respond'])
+        }
+    def jsonToRoute(self, objs):
+        result = []
+        for obj in objs:
+            if 'group' in obj:
+                obj['group'] = self.jsonToRoute(obj['group'])
+                result.append(self.group(**obj))
+            else:
+                result.append(self.route(**obj))
+        return result
+
+class Router:
+    def __init__(self):
+        self.methods = {
+            #"get": {"controller": ..., middleware: []},
+            #"post": {},
+            #"ws": {},
+        }
+        self.sub = {
+            #"sub_url": router
+        }
+        self.sub_regrex = [
+            #(regex, variables, router, ) # regex = reroute
+        ]
+    def search(self, url, method='get', isGetRespond = False):
+        if type(url) == str:
+            method = method.lower()
+            url = url.split('/')
+            while True:
+                try:
+                    url.remove('')
+                except:
+                    break
+        if len(url) == 0:
+            if method in self.methods:
+                if isGetRespond:
+                    return self.methods[method]['middleware'], self.methods[method]['controller'], {}, self.methods[method]['respond']
+                else:
+                    return self.methods[method]['middleware'], self.methods[method]['controller'], {}
+            else: return ([], None, {}, None) if isGetRespond else ([], None, {})
+        current_url = url.pop(0)
+        if current_url in self.sub:
+            result = self.sub[current_url].search(url, method, isGetRespond=isGetRespond)
+            if result[1]: #found
+                return result
+        
+        for regex,variables,_router in self.sub_regrex:
+            matchObj = re.match('^'+regex+'$', current_url)
+            if matchObj:
+                data = {}
+                i = 0
+                for key in variables:
+                    i+=1
+                    data[key] = matchObj.group(i)
+                result = _router.search(url, method, isGetRespond=isGetRespond)
+                if result[1]: #found
+                    result[2].update(data)
+                    return result
+        return ([], None, {}, None) if isGetRespond else ([], None, {})
+        
+    def createRoute(self, url, methods, controller, middleware, respond = None):
+        if len(url) == 0:
+            for method in methods:
+                method = method.lower()
+                if not method in self.methods:
+                    self.methods[method] = {'controller': controller, 'middleware': [], 'respond': respond}
+                for mw in self.methods[method]['middleware']:
+                    if mw in middleware:
+                        middleware.remove(mw)
+                self.methods[method]['middleware'].extend(middleware)
+        else: 
+            current_url = url.pop(0)
+            if re.search(r'{[^{}?]+\??([^{}]+({\d+\,?\d*})?)*}', current_url):
+                
+                def dashrepl(matchobj):
+                    m = re.match(r"{[^{}?]+\?((?:[^{}]+(?:{\d+\,?\d*})?)+)}", matchobj.group(0))
+                    if m:
+                        return '('+m.group(1).replace('(', '(?:')+')'
+                    else:
+                        return '(.*)'
+                def replaceSpesChar(s):
+                    spesChar = ['(',')','[',']','<','>','?','+','\\','*','.','!','$','^','|']
+                    level = 0
+                    result = ''
+                    for c in s:
+                        if c == '{': level+=1
+                        elif c == '}': level-=1
+                        if level==0 and c in spesChar:
+                            result += '\\'
+                        result += c
+                    return result
+                regex_current_url = replaceSpesChar(current_url)
+                revar = re.sub(r'{[^{}?]+\??([^{}]+({\d+\,?\d*})?)*}', '{([^{}?]+)\??(?:[^{}]+(?:{\\\d+\,?\\\d*})?)*}', regex_current_url)
+                variables = []
+                m = re.match(r'^'+revar+'$', current_url)
+                if m:
+                    i = 0
+                    while True:
+                        try:
+                            i+=1
+                            variables.append(m.group(i))
+                        except:
+                            break
+                regex = re.sub(r'{[^{}?]+\??([^{}]+({\d+\,?\d*})?)*}', dashrepl, regex_current_url)
+                tmp = (regex, variables, Router())
+                self.sub_regrex.append(tmp)
+                tmp[2].createRoute(url, methods, controller, middleware, respond)
+            else:
+                if not current_url in self.sub:
+                    self.sub[current_url] = Router()
+                self.sub[current_url].createRoute(url, methods, controller, middleware, respond)
+        return self
+
+class BaseRoute:
+    controllerCache = {}
+    middlewareCache = {}
+    def __init__(self, app_module, isWsRoute = False):
+        self.router = Router()
+        self.isWsRoute = isWsRoute
+        self.Controller = app_module.Controller
+        self.Middleware = app_module.Middleware
+        self.Databases = {}
+        self.Models = {}
+        self.Modules = {}
+        self.Services = {}
+        self.appPath = app_module.__path__[0]
+
+    def generateRoute(self, route_config):
+        self.group(route_config)
+    def group(self, route_config, parent_url = '/', conf_middleware=[]):
+        for conf in route_config:
+            if "controller" in conf:
+                url_arr = (parent_url+'/'+conf['url']).split('/')
+                while True:
+                    try:
+                        url_arr.remove('')
+                    except:
+                        break
+                controller = self.controllerToCallable(conf['controller'])
+                middleware = []
+                for mw in conf_middleware+conf['middleware']:
+                    mw = self.middlewareToCallable(mw)
+                    if mw not in middleware:
+                        middleware.append(mw)
+                self.router.createRoute(url_arr, conf['methods'], controller, middleware, conf['respond'])
+            elif "sub" in conf:
+                self.group(
+                    conf['sub'], 
+                    (parent_url+'/'+conf['url']), 
+                    conf_middleware+(conf['middleware'] if 'middleware' in conf else [])
+                )
+    def controllerToCallable(self, controller):
+        if type(controller) == str:
+            controller = controller.split('@', 1)
+            controller_class_name = controller[0].split('/')
+            if not controller[0] in self.controllerCache:
+                controller_class = self.Controller
+                for c_class_name in controller_class_name:
+                    controller_class = getattr(controller_class, c_class_name)
+                controller_class.appPath = self.appPath
+                controller_class.Databases = self.Databases
+                controller_class.Models = self.Models
+                controller_class.Services = self.Services
+                self.controllerCache[controller[0]] = controller_class()
+
+            return getattr(self.controllerCache[controller[0]], controller[1])
+        elif callable(controller):
+            return controller
+        return None
+    
+    def middlewareToCallable(self, middleware):
+        if type(middleware) is str:
+            middleware = middleware.split('@', 1)
+            middleware_class_name = middleware[0].split('/')
+            if not middleware[0] in self.middlewareCache:
+                middleware_class = self.Middleware
+                for mw_class_name in middleware_class_name:
+                    middleware_class = getattr(middleware_class, mw_class_name)
+                
+                middleware_class.appPath = self.appPath
+                middleware_class.Databases = self.Databases
+                middleware_class.Models = self.Models
+                middleware_class.Services = self.Services
+                self.middlewareCache[middleware[0]] = middleware_class()
+            return getattr(self.middlewareCache[middleware[0]], middleware[1])
+        elif callable(middleware):
+            return middleware
+        return None
