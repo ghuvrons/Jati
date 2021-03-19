539fd648fe1667a83762835204c315f8f63b3b15
ghuvrons <ghuvrons@gmail.com>
2021-03-19 16:08:56 +0700

    cli start

--- Jati/CLI/Serve.py
+++ Jati/CLI/Serve.py
@@ -1,13 +1,13 @@
 import sys, os, Jati
 
 class Serve:
-    def run(self, host = "127.0.0.1", port=3000, isSSL = False, log_file = None):
+    def run(self, host, port, sites, log_file, isSSL):
         current_dir = os.getcwd()
         if current_dir not in sys.path:
             sys.path.insert(1, current_dir)
         try:
-            jati = Jati.Jati(host=host, port=port, isSSL=isSSL, log_file=log_file)
-            jati.addVHost({'localhost': 'localhost'})
+            jati = Jati.Jati(host=host, port=port, isSSL=None, log_file=log_file)
+            jati.addVHost(sites)
             jati.start()
         except KeyboardInterrupt:
             print("closing")
