e9897465be6fc5c94d135baa481cc22930cbc4d7
ghuvrons <ghuvrons@gmail.com>
2021-03-27 15:37:55 +0700

    basic auth

--- Jati/Base/Auth.py
+++ Jati/Base/Auth.py
@@ -4,10 +4,10 @@
 class Auth:
     def __init__(self, user=None): 
         self.user = user # this is user model
+        self.roles = []
 
     def generateToken(self):
         return True
-
 
 class AuthHandler:
     def __init__(self, secretKey):
@@ -17,8 +17,15 @@ class AuthHandler:
     def setUserModel(self):
         self.userModel = userModel
 
-    def authenticate(self, type, token):
-        auth = Auth
-        id = 1
-        auth.user = self.userModel(id)
+    def authenticate(self, authType, token):
+        auth = Auth()
+        user = None
+        if authType == 'Basic':
+            user_id, user_key = base64.b64decode(token.encode('UTF8')).decode('UTF8').split(":", 1)
+            users = self.userModel.search(id=user_id, key=user_key).limit(1)
+            for u in users:
+                user = u
+        else if authType == 'Bearer':
+            pass
+        auth.user = user
         return auth
