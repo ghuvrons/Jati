e547931d63e44768d9aa355cc0e6ae7f398fbce1
ghuvrons <ghuvrons@gmail.com>
2021-04-22 14:48:05 +0700

    sql model and auth generate token

--- Jati/Database/SQL/mysql/Query.py
+++ Jati/Database/SQL/mysql/Query.py
@@ -4,7 +4,4 @@ from ..Query import Query as baseQuery
 
 class Query(baseQuery):
     def getResult(self):
-        if self.result.result is not None:
-            return self.result
-        if self.isSelectQuery:
-            return self.db.execute(self)
+        return self.db.execute(self)
